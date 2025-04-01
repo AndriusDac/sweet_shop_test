8b4a5726548eba417f1560c059714b45f3bf8f24
AndriusDac <jaad75@gmail.com>
2025-04-01 15:21:50 +0300

    Modified header.cy.js

--- cypress/e2e/header.cy.js
+++ cypress/e2e/header.cy.js
@@ -1,146 +1,146 @@
 
-describe('TS02 Header', () => {
-  describe('TS02 Navigation menu from Home page', () => {
+describe('TS_2 Header', () => {
+  describe('TS_2.1 Navigation menu from Home page', () => {
     beforeEach(() => {
       cy.visit("https://sweetshop.netlify.app/");
     });
 
-    it('TC_02.01 Test open Sweets page', () => {
+    it('TC_2.1.1 Test open Sweets page', () => {
       cy.contains('nav a', 'Sweets').click();
       cy.url().should('include', '/sweets');
       cy.get('h1').should('contain', 'Browse sweets');
     });
 
-    it('TC_02.02 Test open About page', () => {
+    it('TC_2.1.2 Test open About page', () => {
       cy.contains('nav a', 'About').click();
       cy.url().should('include', '/about');
       cy.get('.nav-link').should('contain', 'About');
     });
 
-    it('TC_02.03 Test open Login page', () => {
+    it('TC_2.1.3 Test open Login page', () => {
       cy.contains('nav a', 'Login').click();
       cy.url().should('include', '/login');
       cy.get('.nav-link').should('contain', 'Login');
     });
 
-    it('TC_02.04 Test open Basket page', () => {
+    it('TC_2.1.4 Test open Basket page', () => {
       cy.contains('nav a', 'Basket').click();
       cy.url().should('include', '/basket');
       cy.get('.nav-link').should('contain', 'Basket');
     });
   });
 
-  describe('TS02 Navigation menu from Sweets page', () => {
+  describe('TS_2.2 Navigation menu from Sweets page', () => {
     beforeEach(() => {
       cy.visit("https://sweetshop.netlify.app/sweets");
     });
 
-    it('TC_02.05 Test open Home page', () => {
+    it('TC_2.2.1 Test open Home page', () => {
       cy.contains('nav a', 'Sweet Shop').click();
       cy.url().should('eq', 'https://sweetshop.netlify.app/');
     });
 
-    it('TC_02.06 Test open About page', () => {
+    it('TC_2.2.2 Test open About page', () => {
       cy.contains('nav a', 'About').click();
       cy.url().should('include', '/about');
       cy.get('.nav-link').should('contain', 'About');
     });
 
-    it('TC_02.07 Test open Login page', () => {
+    it('TC_2.2.3 Test open Login page', () => {
       cy.contains('nav a', 'Login').click();
       cy.url().should('include', '/login');
       cy.get('.nav-link').should('contain', 'Login');
     });
 
-    it('TC_02.08 Test open Basket page', () => {
+    it('TC_2.2.4 Test open Basket page', () => {
       cy.contains('nav a', 'Basket').click();
       cy.url().should('include', '/basket');
       cy.get('.nav-link').should('contain', 'Basket');
     });
   });
 
-  describe('TS02 Navigation menu from About page', () => {
+  describe('TS_2.3 Navigation menu from About page', () => {
     beforeEach(() => {
       cy.visit("https://sweetshop.netlify.app/about");
     });
 
-    it('TC_02.09 Test open Home page', () => {
+    it('TC_2.3.1 Test open Home page', () => {
       cy.contains('nav a', 'Sweet Shop').click();
       cy.url().should('eq', 'https://sweetshop.netlify.app/');
     });
 
-    it('TC_02.10 Test open Sweets page', () => {
+    it('TC_2.3.2 Test open Sweets page', () => {
       cy.contains('nav a', 'Sweets').click();
       cy.url().should('include', '/sweets');
       cy.get('.nav-link').should('contain', 'Sweets');
     });
 
-    it('TC_02.11 Test open Login page', () => {
+    it('TC_2.3.3 Test open Login page', () => {
       cy.contains('nav a', 'Login').click();
       cy.url().should('include', '/login');
       cy.get('.nav-link').should('contain', 'Login');
     });
 
-    it('TC_02.12 Test open Basket page', () => {
+    it('TC_2.3.4 Test open Basket page', () => {
       cy.contains('nav a', 'Basket').click();
       cy.url().should('include', '/basket');
       cy.get('.nav-link').should('contain', 'Basket');
     });
   });
 
-  describe('TS02 Navigation menu from Login page', () => {
+  describe('TS_2.4 Navigation menu from Login page', () => {
     beforeEach(() => {
       cy.visit("https://sweetshop.netlify.app/login");
     });
 
-    it('TC_02.13 Test open Home page', () => {
+    it('TC_2.4.1 Test open Home page', () => {
       cy.contains('nav a', 'Sweet Shop').click();
       cy.url().should('eq', 'https://sweetshop.netlify.app/');
     });
 
-    it('TC_02.14 Test open Sweets page', () => {
+    it('TC_2.4.2 Test open Sweets page', () => {
       cy.contains('nav a', 'Sweets').click();
       cy.url().should('include', '/sweets');
       cy.get('.nav-link').should('contain', 'Sweets');
     });
 
-    it('TC_02.15 Test open About page', () => {
+    it('TC_2.4.3 Test open About page', () => {
       cy.contains('nav a', 'About').click();
       cy.url().should('include', '/about');
       cy.get('.nav-link').should('contain', 'About');
     });
 
-    it('TC_02.16 Test open Basket page', () => {
+    it('TC_2.4.4 Test open Basket page', () => {
       cy.contains('nav a', 'Basket').click();
       cy.url().should('include', '/basket');
       cy.get('.nav-link').should('contain', 'Basket');
     });
   });
 
-  describe('TS02 Navigation menu from Basket page', () => {
+  describe('TS_2.5 Navigation menu from Basket page', () => {
     beforeEach(() => {
       cy.visit("https://sweetshop.netlify.app/basket");
     });
 
-    it('TC_02.17 Test open Home page', () => {
+    it('TC_2.5.1 Test open Home page', () => {
       cy.contains('nav a', 'Sweet Shop').click();
       cy.url().should('eq', 'https://sweetshop.netlify.app/');
       cy.get('.navbar-brand').should('contain', 'Sweet Shop');
     });
 
-    it('TC_02.18 Test open Sweets page', () => {
+    it('TC_2.5.2 Test open Sweets page', () => {
       cy.contains('nav a', 'Sweets').click();
       cy.url().should('include', '/sweets');
       cy.get('.nav-link').should('contain', 'Sweets');
     });
 
-    it('TC_02.19 Test open About page', () => {
+    it('TC_2.5.3 Test open About page', () => {
       cy.contains('nav a', 'About').click();
       cy.url().should('include', '/about');
       cy.get('.nav-link').should('contain', 'About');
     });
 
-    it('TC_02.20 Test open Login page', () => {
+    it('TC_2.5.4 Test open Login page', () => {
       cy.contains('nav a', 'Login').click();
       cy.url().should('include', '/login');
       cy.get('.nav-link').should('contain', 'Login');

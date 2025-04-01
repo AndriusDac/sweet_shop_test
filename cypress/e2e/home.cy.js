8a7c0f49d1a4c8b4cb323a8245d4d145487575e3
AndriusDac <jaad75@gmail.com>
2025-04-01 16:38:54 +0300

    Modified home.cy.js

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,22 +1,3 @@
-// describe('TC_1.1 Verify that the homepage loads successfully', () => {
-//   beforeEach(() => {
-//     cy.visit('https://sweetshop.netlify.app/');
-//   });
-
-//   it('should load the homepage without errors and display all elements correctly', () => {
-//     // Check if the logo is visible
-//     cy.get('.navbar-brand').should('be.visible');
-
-//     // Check if the navigation bar is visible
-//     cy.get('.navbar').should('be.visible');
-
-//     // Check if the footer is visible
-//     cy.get('.text-center').should('be.visible');
-
-//     // Check if the product list is visible
-//     cy.get('.m-0').should('be.visible');
-//   });
-// });
 
 describe('TS_1 Homepage', () => {
   beforeEach(() => {
